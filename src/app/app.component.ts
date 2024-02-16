8870f3067438b27185a820a65227b553129b351d
Thiago Brito <britotr@gmail.com>
2024-02-15 23:35:58 -0300

    chore: update lintstagedrc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { RouterModule } from '@angular/router'
 import { LayoutModule } from '@ecommerce/layout'
 
 @Component({
-  standalone: true,
-  imports: [RouterModule, LayoutModule],
-  selector: 'ecommerce-root',
-  template: '<ecommerce-header titleHeader="Ecommerce"></ecommerce-header> <router-outlet></router-outlet>',
+    standalone: true,
+    imports: [RouterModule, LayoutModule],
+    selector: 'ecommerce-root',
+    template: '<ecommerce-header titleHeader="Ecommerce"></ecommerce-header> <router-outlet></router-outlet>',
 })
 export class AppComponent {}
