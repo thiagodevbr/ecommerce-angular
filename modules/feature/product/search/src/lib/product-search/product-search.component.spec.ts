be86506a73217a2a897492b1c71076be3dd6e900
Thiago Brito <britotr@gmail.com>
2024-03-17 15:17:57 -0300

    feat: integrates API

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -1,20 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { ProductSearchComponent } from './product-search.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { mockProducts } from 'modules/data-access/product/src/lib/mocks/product.mock'
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 describe('ProductSearchComponent', () => {
   let component: ProductSearchComponent
   let fixture: ComponentFixture<ProductSearchComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductSearchComponent, BrowserAnimationsModule],
+      imports: [ProductSearchComponent, BrowserAnimationsModule, HttpClientTestingModule],
     }).compileComponents()
 
     fixture = TestBed.createComponent(ProductSearchComponent)
     component = fixture.componentInstance
-    component.products = mockProducts
+    // component.products$ = Observable<Product[]>
     fixture.detectChanges()
   })
 
--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatAutocompleteModule } from '@angular/material/autocomplete'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { mockProducts } from '@ecommerce/product-data-access'
+import { ProductSearchService } from '@ecommerce/product-data-access'
 import { Product } from 'modules/data-access/product/src/lib/models/product.model'
+import { Observable, debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs'
 
 @Component({
   selector: 'ecommerce-product-search',
@@ -14,7 +15,19 @@ import { Product } from 'modules/data-access/product/src/lib/models/product.mode
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
 })
-export class ProductSearchComponent {
-  searchFormControl = new FormControl('')
-  products: Product[] = mockProducts
+export class ProductSearchComponent implements OnInit {
+  searchFormControl = new FormControl('', { nonNullable: true })
+  products$!: Observable<Product[]>
+
+  constructor(private productSearchService: ProductSearchService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.searchFormControl.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      filter((text: string) => text.length > 3),
+      // map(text => text + 'TESTE AQUI'),
+      switchMap((text: string) => this.productSearchService.getProductsByName(text)),
+    )
+  }
 }
