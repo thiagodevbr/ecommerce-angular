1fa59fef23b07d58c7305599dc58cb8e380a77ca
Thiago Brito <britotr@gmail.com>
2024-03-17 21:48:02 -0300

    feat: testing product

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -1,23 +1,135 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing'
 import { ProductSearchComponent } from './product-search.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { HttpClientTestingModule } from '@angular/common/http/testing'
-describe('ProductSearchComponent', () => {
+import { ProductSearchService, mockProducts } from '@ecommerce/product-data-access'
+import { Product } from 'modules/data-access/product/src/lib/models/product.model'
+import { Observable, of } from 'rxjs'
+
+describe('#ProductSearchComponent', () => {
   let component: ProductSearchComponent
   let fixture: ComponentFixture<ProductSearchComponent>
+  let productSearchService: ProductSearchService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductSearchComponent, BrowserAnimationsModule, HttpClientTestingModule],
+      imports: [ProductSearchComponent, BrowserAnimationsModule],
+      providers: [
+        {
+          provide: ProductSearchService,
+          useValue: {
+            getProductsByName: (): Observable<Product[]> => of(mockProducts),
+          },
+        },
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(ProductSearchComponent)
     component = fixture.componentInstance
-    // component.products$ = Observable<Product[]>
+    productSearchService = TestBed.inject(ProductSearchService)
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
   })
+
+  it('should debounce when input field is changed', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'getProductsByName')
+    const input: HTMLInputElement = fixture.nativeElement.querySelector('input')
+
+    // Disparo de evento para simular a entrada da palavra shoes no input
+    input.value = 'shoes'
+    input.dispatchEvent(new Event('input'))
+
+    tick(500)
+    expect(productSearchService.getProductsByName).toHaveBeenCalledWith(input.value)
+  }))
+
+  it('should not debounce when input field is changed immediatally', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'getProductsByName')
+    const input: HTMLInputElement = fixture.nativeElement.querySelector('input')
+
+    // Disparo de evento para simular a entrada da palavra shoes no input
+    input.value = 'shoes'
+    input.dispatchEvent(new Event('input'))
+    expect(productSearchService.getProductsByName).not.toHaveBeenCalled()
+    tick(500)
+  }))
+
+  it('should search multiple times', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'getProductsByName')
+    const input: HTMLInputElement = fixture.nativeElement.querySelector('input')
+
+    input.value = 'shoes'
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    input.value = 'pants'
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    input.value = 'mouse'
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    expect(productSearchService.getProductsByName).toHaveBeenCalledTimes(3)
+  }))
+
+  it('should not search if value have length less than 3', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'getProductsByName')
+    const input: HTMLInputElement = fixture.nativeElement.querySelector('input')
+
+    input.value = 'sho'
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    expect(productSearchService.getProductsByName).toHaveBeenCalledTimes(0)
+  }))
+
+  it('should search if value have length plus than 3', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'getProductsByName')
+    const input: HTMLInputElement = fixture.nativeElement.querySelector('input')
+
+    input.value = 'shoes'
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    expect(productSearchService.getProductsByName).toHaveBeenCalledTimes(1)
+  }))
+
+  it('should not search multiple times if value have same value', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'getProductsByName')
+    const input: HTMLInputElement = fixture.nativeElement.querySelector('input')
+
+    input.value = 'shoes'
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    input.value = 'shoes'
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    input.value = 'shoes'
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    expect(productSearchService.getProductsByName).toHaveBeenCalledTimes(1)
+  }))
+
+  it('should not search if empty value', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'getProductsByName')
+    const input: HTMLInputElement = fixture.nativeElement.querySelector('input')
+
+    input.value = ''
+    input.dispatchEvent(new Event('input'))
+    tick(500)
+
+    expect(productSearchService.getProductsByName).toHaveBeenCalledTimes(0)
+  }))
+
+  it('should return products observable correctly', fakeAsync(() => {
+    component.products$.subscribe((products: Product[]) => {
+      expect(products).toEqual(mockProducts)
+    })
+  }))
 })
